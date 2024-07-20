1ec38b49e1d386bb6f917a50b80993a7963703b8
Mohamed Sarda <cbamiixsimo@gmail.com>
2024-07-20 20:31:48 +0100

    idk

--- recasting/recasting.c
+++ recasting/recasting.c
@@ -162,202 +162,253 @@ int has_wall(t_cub *cube, double x1, double y1)
     return (0);
 }
 
-float normilizeangle(t_cub *cube)
-{
-    
-    cube->player->rotat_angle = remainder(cube->player->rotat_angle, M_PI);
-    if (cube->player->rotat_angle < 0)
-        cube->player->rotat_angle = (2 * M_PI) + cube->player->rotat_angle;
-    return (cube->player->rotat_angle);
-}
-
-void draw_line(t_cub *cube, int circle_center_x, int circle_center_y, double j)
-{
-
-    // double israyfacing_down = 0;
-    // if(cube->player->rotat_angle > 0 && cube->player->rotat_angle < M_PI)
-    //     israyfacing_down = 1;
-    // double israyfacing_up = !israyfacing_down;
-    // double israyfacing_right = 0;
-    // if(cube->player->rotat_angle <0.5 * M_PI  || cube->player->rotat_angle > 1.5 * M_PI)
-    //     israyfacing_right = 1;
-    // double israyfacing_left = !israyfacing_right;
-
-    int new_line_end_x;
-    int new_line_end_y;
-    
-// first cube x_y
-    // double y_intercept = floor(circle_center_y/ tile_size) * tile_size;
-    // if(israyfacing_down == 1)
-    //     y_intercept += tile_size;
-    // double x_intercept = circle_center_x + ((circle_center_y - y_intercept) / tan(cube->player->rotat_angle));
-    // printf("---- y_intercept = %f | x_intercept = %f\n", y_intercept , x_intercept);
-// end first cube x_y
-
-// steps
-    // double ystep = tile_size;
-    // if(israyfacing_up == 1)
-    //     ystep *= -1;
-    // else
-    //     ystep *= 1;
-    // double xstep = tile_size / tan(cube->player->rotat_angle);
-    // if(israyfacing_left == 1 && xstep > 0)
-    //     xstep *= -1;
-    // else
-    //     xstep *= 1;
-    // if(israyfacing_right == 1 && xstep < 0)
-    //     xstep *= -1;
-    // else
-    //     xstep *= 1;
-    // printf("++++ ystep = %f | xstep =  %f\n", ystep , xstep);
-// done steps
-
-    // double nexthorz_x = x_intercept;
-    // double nexthorz_y = y_intercept;
-// 
-    // if(israyfacing_up == 1)
-    //     nexthorz_y--;
-    // while (/* condition */)
-    // {
-    //     if(has_wall(cube, nexthorz_x, nexthorz_y))
-    //     {
-// 
-    //     }
-    //     else
-    //     {
-    //         nexthorz_x += xstep;
-    //         nexthorz_y += ystep;
-    //     }
-// 
-    // }
-    
-    // if(israyfacing_up == 1)
-    //     y_intercept--;
-    // double start_x =  x_intercept;
-    // double start_y =  y_intercept;
-
-    // printf("%d, %d\n", a, b);
-
-// up down
-    // while (start_x >= 0 && start_x <= WIDTH && start_y >= 0 && start_y <= HEIGHT)
-    // {
-    //     int a = floor(start_x / tile_size);
-    //     int b = floor(start_y / tile_size);
-    //     // printf("%d, %d\n", a, b);
-    //     if(cube->map[b][a] == 1)
-    //     {
-    //         // puts("++++");
-    //         break;
-    //     }
-    //     start_y += ystep;
-    //     start_x += xstep;
-    //     printf("[%f] | [%f]\n+++++ \n", start_y, start_x);
-    //     // b -= 1;
-    // }
-    // start_y--;
-    // double start_y = b * tile_size;    
-    // if(israyfacing_up == 1)
-    //     start_y =  circle_center_y - start_y;
-    // else if(israyfacing_down == 1)
-    //     start_y =  start_y - circle_center_y;
-    // else if(israyfacing_left == 1)
-    //     start_y =  circle_center_x - start_x;
-    // else if(israyfacing_right == 1)
-    //     start_y =  start_x - circle_center_x;
-    // new_line_end_x = (circle_center_x  + (start_x * cos(j)));
-    // new_line_end_y = (circle_center_y  + (start_y * sin(j)));
-    // new_line_end_x = (circle_center_x  + (start_x * cos(j)));
-    // new_line_end_y = (circle_center_y  + ((start_y - circle_center_y) * sin(j)));
-//
-  
-    // double y_intercept = 150;
-    // double x_intercept = 525;
-    // double ystep =  50;
-    // double xstep =  0;
-    // double start_x =  x_intercept = 525;
-    // double start_y =  y_intercept = 150;
-    // start_x = x_intercept / tile_size;
-    // start_y = y_intercept / tile_size;
-
-    // int f_x;
-    // int f_y;
-    // while (1)
-    // {
-    //     f_x = start_x / tile_size;
-    //     f_y = start_y / tile_size;
-    //     if(cube->map[f_y][f_x] == 1)
-    //     {
-    //         printf("f-y = %d | f-x = %d\n", f_y, f_x);
-    //         printf("start_y = %f | start_x = %f\n", start_y, start_x);
-    //         break;
-    //     }
-    //     start_x += xstep;
-    //     start_y -= ystep;
-    // }
-    // f_x -= xstep;
-    // f_y -= ystep;
-    // f_x *= tile_size;
-    // f_y *= tile_size;
-
-  
-    new_line_end_x = (circle_center_x  + (line_leng * cos(j)));
-    new_line_end_y = (circle_center_y  + (line_leng * sin(j)));
-
-    DDA(cube, circle_center_x, circle_center_y, new_line_end_x, new_line_end_y);
+double normalizeAngle(double angle) {
+    angle = fmod(angle, 2 * M_PI);
+    if (angle < 0) {
+        angle += 2 * M_PI;
+    }
+    return angle;
 }
 
 // void draw_line(t_cub *cube, int circle_center_x, int circle_center_y, double j)
 // {
+
+//     double israyfacing_down = 0;
+//     if(cube->player->rotat_angle > 0 && cube->player->rotat_angle < M_PI)
+//         israyfacing_down = 1;
+//     double israyfacing_up = !israyfacing_down;
+//     double israyfacing_right = 0;
+//     if(cube->player->rotat_angle <0.5 * M_PI  || cube->player->rotat_angle > 1.5 * M_PI)
+//         israyfacing_right = 1;
+//     double israyfacing_left = !israyfacing_right;
+
 //     int new_line_end_x;
 //     int new_line_end_y;
-//     // new_line_end_x = (circle_center_x  + (line_leng * cos(j)));
-//     // new_line_end_y = (circle_center_y  + (line_leng * sin(j)));
-//     double wallhit_X;
-//     double wallhit_Y;
-//     double is_ray_facing_down = cube->player->rotat_angle > 0 && cube->player->rotat_angle  < M_PI;
-//     double is_ray_facing_UP = !is_ray_facing_down;
-//     double is_ray_facing_right = cube->player->rotat_angle < 0.5 * M_PI || cube->player->rotat_angle > 1.5 * M_PI;
-//     double is_ray_facing_left = !is_ray_facing_right;
-// 
+    
+// // first cube x_y
 //     double y_intercept = floor(circle_center_y/ tile_size) * tile_size;
-//     y_intercept += is_ray_facing_down ? tile_size : 0;
+//     if(israyfacing_down == 1)
+//         y_intercept += tile_size;
 //     double x_intercept = circle_center_x + ((circle_center_y - y_intercept) / tan(cube->player->rotat_angle));
-// 
-//     double ystep = tile_size;
-//     ystep *= is_ray_facing_down ? -1 : 1;
-//     double xstep = tile_size / tan(cube->player->rotat_angle);
-//     xstep *= (is_ray_facing_left && xstep > 0) ? -1 : 1;
-//     xstep *= (is_ray_facing_right && xstep < 0) ? -1 : 1;
-//     // printf("x = [%f] || y = [%f] ||p_x = [%d] || p_y = [%d] || [%f]\n", x, y, circle_center_x, circle_center_y, circle_center_y - y);
-//     // new_line_end_x = (x  + (line_leng * cos(j)));
-//     // new_line_end_y = ((circle_center_y - y) + (line_leng * sin(j)));
-//     double next_horz_touchX = x_intercept;
-//     double next_horz_touchY = y_intercept;
-//     if (is_ray_facing_UP)
-//         next_horz_touchY--;
-//     int i = 0;
-//     while (i < 1)
-//     {
-//         if(has_wall(cube, next_horz_touchX, next_horz_touchY) == 1)
-//         {
-//             wallhit_X = next_horz_touchX;
-//             wallhit_Y = next_horz_touchY;
-//             break;
-//         }
-//         else
-//         {
-//             next_horz_touchX += xstep;
-//             next_horz_touchY += ystep;
-//         }
-//         i++;
-//     }
+//     // printf("---- y_intercept = %f | x_intercept = %f\n", y_intercept , x_intercept);
+//     // printf("---- y_player = %d | x_player = %d\n", circle_center_y , circle_center_x);
+// // end first cube x_y
 
-//     // new_line_end_x = (xstep  + (line_leng * cos(j)));
-//     // new_line_end_y = (ystep  + (line_leng * sin(j)));
+// // steps
+//     double ystep = tile_size;
+//     if(israyfacing_up == 1)
+//         ystep *= -1;
+//     else
+//         ystep *= 1;
+//     double xstep = tile_size / tan(cube->player->rotat_angle);
+//     if(israyfacing_left == 1 && xstep > 0)
+//         xstep *= -1;
+//     else
+//         xstep *= 1;
+//     if(israyfacing_right == 1 && xstep < 0)
+//         xstep *= -1;
+//     else
+//         xstep *= 1;
+//     // printf("++++ ystep = %f | xstep =  %f\n", ystep , xstep);
+// // done steps
+
+//     // double nexthorz_x = x_intercept;
+//     // double nexthorz_y = y_intercept;
+// // 
+//     // if(israyfacing_up == 1)
+//     //     nexthorz_y--;
+//     // while (/* condition */)
+//     // {
+//     //     if(has_wall(cube, nexthorz_x, nexthorz_y))
+//     //     {
+// // 
+//     //     }
+//     //     else
+//     //     {
+//     //         nexthorz_x += xstep;
+//     //         nexthorz_y += ystep;
+//     //     }
+// // 
+//     // }
+    
+//     // if(israyfacing_up == 1)
+//     //     y_intercept--;
+//     // double start_x =  x_intercept;
+//     // double start_y =  y_intercept;
+
+//     // printf("%d, %d\n", a, b);
+
+// // up down
+//     // while (start_x >= 0 && start_x <= WIDTH && start_y >= 0 && start_y <= HEIGHT)
+//     // {
+//     //     int a = floor(start_x / tile_size);
+//     //     int b = floor(start_y / tile_size);
+//     //     // printf("%d, %d\n", a, b);
+//     //     if(cube->map[b][a] == '1')
+//     //     {
+//     //         // puts("++++");
+//     //         break;
+//     //     }
+//     //     start_y += ystep;
+//     //     start_x += xstep;
+//     //     printf("[%f] | [%f]\n+++++ \n", start_y, start_x);
+//     //     // b -= 1;
+//     // }
+//     // // start_y -= ystep;
+//     // // double start_y = tile_size;    
+//     // if(israyfacing_up == 1)
+//     //     start_y =  circle_center_y - start_y;
+//     // else if(israyfacing_down == 1)
+//     //     start_y =  start_y - circle_center_y;
+//     // else if(israyfacing_left == 1)
+//     //     start_y =  circle_center_x - start_x;
+//     // else if(israyfacing_right == 1)
+//     //     start_y =  start_x - circle_center_x;
+//     // new_line_end_x = (circle_center_x  + (start_x * cos(j)));
+//     // new_line_end_y = (circle_center_y  + ((start_y) * sin(j)));
+//     // new_line_end_x = (circle_center_x  + (start_x * cos(j)));
+//     // new_line_end_y = (circle_center_y  + ((start_y - circle_center_y) * sin(j)));
+// //
+  
+//     // double y_intercept = 150;
+//     // double x_intercept = 525;
+//     // double ystep =  50;
+//     // double xstep =  0;
+//     // double start_x =  x_intercept = 525;
+//     // double start_y =  y_intercept = 150;
+//     // start_x = x_intercept / tile_size;
+//     // start_y = y_intercept / tile_size;
+
+//     // int f_x;
+//     // int f_y;
+//     // while (1)
+//     // {
+//     //     f_x = start_x / tile_size;
+//     //     f_y = start_y / tile_size;
+//     //     if(cube->map[f_y][f_x] == 1)
+//     //     {
+//     //         printf("f-y = %d | f-x = %d\n", f_y, f_x);
+//     //         printf("start_y = %f | start_x = %f\n", start_y, start_x);
+//     //         break;
+//     //     }
+//     //     start_x += xstep;
+//     //     start_y -= ystep;
+//     // }
+//     // f_x -= xstep;
+//     // f_y -= ystep;
+//     // f_x *= tile_size;
+//     // f_y *= tile_size;
+
+//     // printf("[%d] | [%f]\n", circle_center_y, y_intercept);
+//     new_line_end_x = (circle_center_x  + (x_intercept * cos(j)));
+//     new_line_end_y = (circle_center_y  + ((circle_center_y - y_intercept) * sin(j)));
 
 //     DDA(cube, circle_center_x, circle_center_y, new_line_end_x, new_line_end_y);
 // }
 
+double distanceBetweenPoints(x1, y1, x2, y2) {
+    return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
+
+void draw_line(t_cub *cube, int circle_center_x, int circle_center_y, double angle)
+{
+    (void)angle;
+    cube->player->rotat_angle = normalizeAngle(cube->player->rotat_angle);
+
+    int isRayFacingDown = cube->player->rotat_angle > 0 && cube->player->rotat_angle < M_PI;
+    int isRayFacingUp = !isRayFacingDown;
+    int isRayFacingRight = cube->player->rotat_angle < 0.5 * M_PI || cube->player->rotat_angle > 1.5 * M_PI;
+    int isRayFacingLeft = !isRayFacingRight;
+
+    ///////////////////////////////////////////
+    // HORIZONTAL RAY-GRID INTERSECTION CODE
+    ///////////////////////////////////////////
+    int foundHorzWallHit = 0;
+    double horzWallHitX = 0;
+    double horzWallHitY = 0;
+
+    double y_intercept = floor(circle_center_y / tile_size) * tile_size;
+    y_intercept += isRayFacingDown ? tile_size : 0;
+
+    double x_intercept = circle_center_x + (y_intercept - circle_center_y) / tan(cube->player->rotat_angle);
+
+    double ystep = tile_size;
+    ystep *= isRayFacingUp ? -1 : 1;
+
+    double xstep = tile_size / tan(cube->player->rotat_angle);
+    xstep *= (isRayFacingLeft && xstep > 0) ? -1 : 1;
+    xstep *= (isRayFacingRight && xstep < 0) ? -1 : 1;
+
+    double nextHorzTouchX = x_intercept;
+    double nextHorzTouchY = y_intercept;
+
+    if (isRayFacingUp)
+        nextHorzTouchY--;
+
+    while (nextHorzTouchX >= 0 && nextHorzTouchX <= WIDTH && nextHorzTouchY >= 0 && nextHorzTouchY <= HEIGHT) {
+        if (has_wall(cube, nextHorzTouchX, nextHorzTouchY)) {
+            foundHorzWallHit = 1;
+            horzWallHitX = nextHorzTouchX;
+            horzWallHitY = nextHorzTouchY;
+            break;
+        } else
+        {
+            nextHorzTouchX += xstep;
+            nextHorzTouchY += ystep;
+        }
+    }
+
+    ///////////////////////////////////////////
+    // VERTICAL RAY-GRID INTERSECTION CODE
+    ///////////////////////////////////////////
+    int foundVertWallHit = 0;
+    double vertWallHitX = 0;
+    double vertWallHitY = 0;
+
+    x_intercept = floor(circle_center_x / tile_size) * tile_size;
+    x_intercept += isRayFacingRight ? tile_size : 0;
+
+    y_intercept = circle_center_y + (x_intercept - circle_center_x) * tan(cube->player->rotat_angle);
+
+    xstep = tile_size;
+    xstep *= isRayFacingLeft ? -1 : 1;
+
+    ystep = tile_size * tan(cube->player->rotat_angle);
+    ystep *= (isRayFacingUp && ystep > 0) ? -1 : 1;
+    ystep *= (isRayFacingDown && ystep < 0) ? -1 : 1;
+
+    double nextVertTouchX = x_intercept;
+    double nextVertTouchY = y_intercept;
+
+    if (isRayFacingLeft)
+        nextVertTouchX--;
+
+    while (nextVertTouchX >= 0 && nextVertTouchX <= WIDTH && nextVertTouchY >= 0 && nextVertTouchY <= HEIGHT) {
+        if (has_wall(cube, nextVertTouchX, nextVertTouchY)) {
+            foundVertWallHit = 1;
+            vertWallHitX = nextVertTouchX;
+            vertWallHitY = nextVertTouchY;
+            break;
+        } else {
+            nextVertTouchX += xstep;
+            nextVertTouchY += ystep;
+        }
+    }
+
+    // Calculate both horizontal and vertical distances and choose the smallest value
+    double horzHitDistance = (foundHorzWallHit)
+        ? distanceBetweenPoints(circle_center_x, circle_center_y, horzWallHitX, horzWallHitY)
+        : __DBL_MAX__;
+    double vertHitDistance = (foundVertWallHit)
+        ? distanceBetweenPoints(circle_center_x, circle_center_y, vertWallHitX, vertWallHitY)
+        : __DBL_MAX__;
+
+    // Only store the smallest of the distances
+    double wallHitX = (horzHitDistance < vertHitDistance) ? horzWallHitX : vertWallHitX;
+    double wallHitY = (horzHitDistance < vertHitDistance) ? horzWallHitY : vertWallHitY;
+
+    DDA(cube, circle_center_x, circle_center_y, wallHitX, wallHitY);
+}
 void    draw_lines(t_cub *cube, int circle_center_x, int circle_center_y)
 {
     double angle;
@@ -400,7 +451,7 @@ void draw_view_player(t_cub *cube)
     
     draw_player(cube, cube->player->player_x, cube->player->player_y);
     draw_line(cube, cube->player->player_x, cube->player->player_y, cube->player->rotat_angle);
-    draw_lines(cube, cube->player->player_x, cube->player->player_y);
+    // draw_lines(cube, cube->player->player_x, cube->player->player_y);
 }
 
 void	handle_pixel2(int x, int y, t_cub *cube)
@@ -495,7 +546,7 @@ int	handle_close_button(t_cub *data)
 int handle_input_key_down(int keycode, t_cub * data)
 {
     if(keycode == 124)
-        data->player->turn_direction = +1;
+        data->player->turn_direction = 1;
     else if(keycode == 125)
         data->player->walk_direction = -1;
     else if(keycode == 13)
