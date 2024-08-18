e15cba574432eae52de72b0c77900901057ec268
El hossein El ghalbzouri <eel-ghal@e3r9p18.1337.ma>
2024-08-18 09:07:40 +0100

    idk

--- Bonus/recasting/ft_mini_map.c
+++ Bonus/recasting/ft_mini_map.c
@@ -1,40 +1,39 @@
 #include "../../recasting.h"
 
-void ft_draw_map_background(t_cub *cube, int minimap_size, double xx, double yy)
+void ft_draw_map_background(t_cub *cube)
 {
 	int	y;
 	int	x;
-	int	screen_x;
-	int	screen_y;
+	int	sc_x;
+	int	sc_y;
 	int	dx;
 	int	dy;
 
-	y = -(minimap_size / 2);
-	while (y <= (minimap_size / 2))
+	y = -(cube->player->minimap_size / 2);
+	while (y <= (cube->player->minimap_size / 2))
 	{
-		x = -(minimap_size / 2);
-		while (x <= (minimap_size / 2))
+		x = -(cube->player->minimap_size / 2);
+		while (x <= (cube->player->minimap_size / 2))
 		{
-			screen_x = xx + (minimap_size / 2) + x;
-			screen_y = yy + (minimap_size / 2) + y;
-
+			sc_x = cube->player->minimap_start_x + (cube->player->minimap_size / 2) + x;
+			sc_y = cube->player->minimap_start_y + (cube->player->minimap_size / 2) + y;
 			dx = x;
 			dy = y;
-			if(cube->player->tab == 1)
+			if (cube->player->minimap == 1 && cube->player->tab != 1)
 			{
-				if (dx * dx + dy * dy <= (minimap_size / 2) * (minimap_size / 2))
-					mlx_put_pixel(cube->image, screen_x, screen_y, create_rgba(0, 0, 0, 1));
+				if (dx * dx + dy * dy <= (cube->player->minimap_size / 2) * (cube->player->minimap_size / 2))
+					mlx_put_pixel(cube->image, sc_x, sc_y, create_rgba(0, 0, 0, 1));
 			}
 			else
 			{
-				if (dx >= -(minimap_size / 2) && dx <= (minimap_size / 2) &&
-					dy >= -(minimap_size / 2) && dy <= (minimap_size / 2))
-					{
-            			if(y == -(minimap_size / 2) || x == -(minimap_size / 2) || x == (minimap_size / 2) || y == (minimap_size / 2))
-							mlx_put_pixel(cube->image, screen_x, screen_y, create_rgba(0, 0, 0, 255));
-						else
-							mlx_put_pixel(cube->image, screen_x, screen_y, create_rgba(0, 0, 0, 1));
-					}
+				if (dx >= -(cube->player->minimap_size / 2) && dx <= (cube->player->minimap_size / 2)
+					&& dy >= -(cube->player->minimap_size / 2) && dy <= (cube->player->minimap_size / 2))
+				{
+					if (y == -(cube->player->minimap_size / 2) || x == -(cube->player->minimap_size / 2) || x == (cube->player->minimap_size / 2) || y == (cube->player->minimap_size / 2))
+						mlx_put_pixel(cube->image, sc_x, sc_y, create_rgba(0, 0, 0, 255));
+					else
+						mlx_put_pixel(cube->image, sc_x, sc_y, create_rgba(0, 0, 0, 1));
+				}
 			}
 
 			x++;
@@ -43,49 +42,76 @@ void ft_draw_map_background(t_cub *cube, int minimap_size, double xx, double yy)
 	}
 }
 
-void    ft_draw_minimap_to_screen(t_cub *cube, int minimap_size, int minimap_start_x, int minimap_start_y)
+void	init_wh_if(t_cub *cube, int is, int x, int y)
+{
+	if(is == 1)
+	{
+		cube->map_data.world_x = cube->player->player_x + x * 2;
+		cube->map_data.world_y = cube->player->player_y + y * 2;
+		cube->map_data.map_x = floor(cube->map_data.world_x / tile_size);
+		cube->map_data.map_y = floor(cube->map_data.world_y / tile_size);
+	}
+	else
+	{
+		cube->map_data.color = create_rgba(192, 192, 192, 255);
+		if (cube->data->map[cube->map_data.map_y][cube->map_data.map_x] == '1')
+			cube->map_data.color = create_rgba(0, 0, 0, 100);
+		cube->map_data.screen_x = cube->player->minimap_start_x + (cube->player->minimap_size / 2) + x;
+		cube->map_data.screen_y = cube->player->minimap_start_y + (cube->player->minimap_size / 2) + y;
+		cube->map_data.dx = x;
+		cube->map_data.dy = y;
+	}
+}
+
+void	init_wh_if(t_cub *cube)
+{
+	int	a;
+	int	b;
+
+	b = (cube->player->minimap_size / 2) * (cube->player->minimap_size / 2);
+	a = cube->map_data.dx * cube->map_data.dx + cube->map_data.dy
+		* cube->map_data.dy;
+	if (cube->player->minimap == 1 && cube->player->tab != 1)
+	{
+		if (a <= b)
+			mlx_put_pixel(cube->image, cube->map_data.screen_x, cube->map_data.screen_y, cube->map_data.color);
+	}
+	else
+	{
+		if (cube->map_data.dx >= -(cube->player->minimap_size / 2) 
+			&& cube->map_data.dx <= (cube->player->minimap_size / 2)
+			&& cube->map_data.dy >= -(cube->player->minimap_size / 2)
+			&& cube->map_data.dy <= (cube->player->minimap_size / 2))
+			mlx_put_pixel(cube->image, cube->map_data.screen_x, cube->map_data.screen_y, cube->map_data.color);
+	}
+}
+
+void	ft_draw_minimap_to_screen(t_cub *cube)
 {
 	int	y;
 	int	x;
 
-	y = -(minimap_size / 2);
-	while (y <= (minimap_size / 2))
+	y = -(cube->player->minimap_size / 2);
+	while (y <= (cube->player->minimap_size / 2))
 	{
-		x = -(minimap_size / 2);
-		while (x <= (minimap_size / 2))
+		x = -(cube->player->minimap_size / 2);
+		while (x <= (cube->player->minimap_size / 2))
 		{
 			x++;
-			cube->map_data.world_x = cube->player->player_x + x * 2;
-			cube->map_data.world_y = cube->player->player_y + y * 2;
-			cube->map_data.map_x = floor(cube->map_data.world_x / tile_size);
-			cube->map_data.map_y = floor(cube->map_data.world_y / tile_size);
-			if (cube->map_data.map_x >= 0 && cube->map_data.map_x < cube->data->map_cols && cube->map_data.map_y >= 0 && cube->map_data.map_y < cube->data->map_row)
+			init_wh_if(cube, 1, x, y);
+			if (cube->map_data.map_x < cube->data->map_cols
+				&& cube->map_data.map_x >= 0 && cube->map_data.map_y >= 0
+				&& cube->map_data.map_y < cube->data->map_row)
 			{
-				cube->map_data.color = create_rgba(192, 192, 192, 255);
-				if (cube->data->map[cube->map_data.map_y][cube->map_data.map_x] == '1')
-					cube->map_data.color = create_rgba(0, 0, 0, 100);
-				cube->map_data.screen_x = minimap_start_x + (minimap_size / 2) + x;
-				cube->map_data.screen_y = minimap_start_y + (minimap_size / 2) + y;
-				cube->map_data.dx = x;
-				cube->map_data.dy = y;
-				if(cube->player->tab == 1)
-				{
-					if (cube->map_data.dx * cube->map_data.dx + cube->map_data.dy * cube->map_data.dy <= (minimap_size / 2) * (minimap_size / 2))
-						mlx_put_pixel(cube->image, cube->map_data.screen_x, cube->map_data.screen_y, cube->map_data.color);
-				}
-				else
-				{
-					if (cube->map_data.dx >= -(minimap_size / 2) && cube->map_data.dx <= (minimap_size / 2) &&
-						cube->map_data.dy >= -(minimap_size / 2) && cube->map_data.dy <= (minimap_size / 2))
-						mlx_put_pixel(cube->image, cube->map_data.screen_x, cube->map_data.screen_y, cube->map_data.color);
-				}
+				init_wh_if(cube, 2, x, y);
+				check_put_pxl(cube);
 			}
 		}
 		y++;
 	}
 }
 
-void    ft_draw_player(t_cub *cube, int minimap_player_x, int minimap_player_y)
+void	ft_draw_player(t_cub *cube)
 {
 
 	int	player_radius;
@@ -100,42 +126,42 @@ void    ft_draw_player(t_cub *cube, int minimap_player_x, int minimap_player_y)
 		while (i <= player_radius)
 		{
 			if (i * i + j * j <= player_radius * player_radius)
-				mlx_put_pixel(cube->image, minimap_player_x + i * 2, minimap_player_y + j * 2, create_rgba(255, 0, 0, 255));
+				mlx_put_pixel(cube->image, cube->player->minimap_player_x + i * 2, cube->player->minimap_player_y + j * 2, create_rgba(255, 0, 0, 255));
 			i++;
 		}
 		j++;
 	}
 }
 
-void    ft_draw_player_indecator(t_cub *cube, int minimap_player_x, int minimap_player_y)
+void    ft_draw_player_indecator(t_cub *cube)
 {
 	int indicator_length = 20;
-	int end_x = minimap_player_x + cos(cube->player->rotat_angle) * indicator_length;
-	int end_y = minimap_player_y + sin(cube->player->rotat_angle) * indicator_length;
-	int dx = abs(end_x - minimap_player_x);
-	int dy = abs(end_y - minimap_player_y);
-	int sx = minimap_player_x < end_x ? 1 : -1;
-	int sy = minimap_player_y < end_y ? 1 : -1;
+	int end_x = cube->player->minimap_player_x + cos(cube->player->rotat_angle) * indicator_length;
+	int end_y = cube->player->minimap_player_y + sin(cube->player->rotat_angle) * indicator_length;
+	int dx = abs(end_x - cube->player->minimap_player_x);
+	int dy = abs(end_y - cube->player->minimap_player_y);
+	int sx = cube->player->minimap_player_x < end_x ? 1 : -1;
+	int sy = cube->player->minimap_player_y < end_y ? 1 : -1;
 	int err = dx - dy;
 
-	while ((int)minimap_player_x != end_x || (int)minimap_player_y != end_y)
+	while ((int)cube->player->minimap_player_x != end_x || (int)cube->player->minimap_player_y != end_y)
 	{
-		mlx_put_pixel(cube->image, minimap_player_x, minimap_player_y, create_rgba(255, 0, 0, 255));
+		mlx_put_pixel(cube->image, cube->player->minimap_player_x, cube->player->minimap_player_y, create_rgba(255, 0, 0, 255));
 		int e2 = 2 * err;
 		if (e2 > -dy)
 		{
 			err -= dy;
-			minimap_player_x += sx;
+			cube->player->minimap_player_x += sx;
 		}
 		if (e2 < dx)
 		{
 			err += dx;
-			minimap_player_y += sy;
+			cube->player->minimap_player_y += sy;
 		}
 	}
 }
 
-void ft_draw_border(t_cub *cube, int minimap_size, double xx, double yy)
+void ft_draw_border(t_cub *cube)
 {
 	int	y;
 	int	x;
@@ -144,24 +170,23 @@ void ft_draw_border(t_cub *cube, int minimap_size, double xx, double yy)
 	int	dx;
 	int	dy;
 
-	y = -(minimap_size / 2);
-	while (y <= (minimap_size / 2))
+	y = -(cube->player->minimap_size / 2);
+	while (y <= (cube->player->minimap_size / 2))
 	{
-		x = -(minimap_size / 2);
-		while (x <= (minimap_size / 2))
+		x = -(cube->player->minimap_size / 2);
+		while (x <= (cube->player->minimap_size / 2))
 		{
-			screen_x = xx + (minimap_size / 2) + x;
-			screen_y = yy + (minimap_size / 2) + y;
-
+			screen_x = cube->player->minimap_start_x + (cube->player->minimap_size / 2) + x;
+			screen_y = cube->player->minimap_start_y + (cube->player->minimap_size / 2) + y;
 			dx = x;
 			dy = y;
-			if(cube->player->tab != 1)
+			if (cube->player->minimap != 1)
 			{
-				if (dx >= -(minimap_size / 2) && dx <= (minimap_size / 2) &&
-					dy >= -(minimap_size / 2) && dy <= (minimap_size / 2))
+				if (dx >= -(cube->player->minimap_size / 2) && dx <= (cube->player->minimap_size / 2) &&
+					dy >= -(cube->player->minimap_size / 2) && dy <= (cube->player->minimap_size / 2))
 					{
-            			if(y == -(minimap_size / 2) || x == -(minimap_size / 2) || x == (minimap_size / 2) || y == (minimap_size / 2)
-							|| y == -(minimap_size / 2) + 1 || x == -(minimap_size / 2) + 1 || x == (minimap_size / 2) - 1|| y == (minimap_size / 2) - 1)
+						if (y == -(cube->player->minimap_size / 2) || x == -(cube->player->minimap_size / 2) || x == (cube->player->minimap_size / 2) || y == (cube->player->minimap_size / 2)
+							|| y == -(cube->player->minimap_size / 2) + 1 || x == -(cube->player->minimap_size / 2) + 1 || x == (cube->player->minimap_size / 2) - 1|| y == (cube->player->minimap_size / 2) - 1)
 							mlx_put_pixel(cube->image, screen_x, screen_y, create_rgba(0, 0, 0, 255));
 					}
 			}
@@ -174,33 +199,30 @@ void ft_draw_border(t_cub *cube, int minimap_size, double xx, double yy)
 
 void draw_minimap(t_cub *cube)
 {
-	int minimap_size;
-	int minimap_start_x;
-	int minimap_start_y;
-	minimap_start_y = 10;
-	if(cube->player->tab == 1)
+	cube->player->minimap_start_y = 10;
+	if (cube->player->minimap == 1 && cube->player->tab != 1)
 	{
-		minimap_size = 200;
-		minimap_start_x = WIDTH / 1.12;
+		cube->player->minimap_size = 200;
+		cube->player->minimap_start_x = WIDTH / 1.12;
 	}
 	else
 	{
-		minimap_size = 500;
-		minimap_start_x = WIDTH / 1.35;
+		cube->player->minimap_size = 500;
+		cube->player->minimap_start_x = WIDTH / 1.35;
 	}
-	ft_draw_map_background(cube, minimap_size, minimap_start_x, minimap_start_y);
-	ft_draw_minimap_to_screen(cube, minimap_size, minimap_start_x, minimap_start_y);
-	int minimap_player_x = minimap_start_x + (minimap_size / 2);
-	int minimap_player_y = minimap_start_y + (minimap_size / 2);
-	ft_draw_player(cube, minimap_player_x, minimap_player_y);
-	ft_draw_player_indecator(cube, minimap_player_x, minimap_player_y);
-	ft_draw_border(cube, minimap_size, minimap_start_x, minimap_start_y);
+	ft_draw_map_background(cube);
+	ft_draw_minimap_to_screen(cube);
+	cube->player->minimap_player_x = cube->player->minimap_start_x + (cube->player->minimap_size / 2);
+	cube->player->minimap_player_y = cube->player->minimap_start_y + (cube->player->minimap_size / 2);
+	ft_draw_player(cube);
+	ft_draw_player_indecator(cube);
+	ft_draw_border(cube);
 }
 //end draw_player
 
 void	handle_pixel2(int x, int y, t_cub *cube)
 {
-	if(cube->data->map[y][x] == cube->data->p)
+	if (cube->data->map[y][x] == cube->data->p)
 	{
 		draw_minimap(cube);
 	}
--- recasting.h
+++ recasting.h
@@ -1,58 +1,55 @@
 #ifndef RECASTING_H
-#define RECASTING_H
+# define RECASTING_H
 
-#include <math.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-// #include "mlx.h"
-#include "./parsing.h"
-#include "../MLX42/include/MLX42/MLX42.h"
-#define tile_size 64
-// #define map_row 14
-// #define map_cols 33
+# include <math.h>
+# include <stdio.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+// # include "mlx.h"
+# include "./parsing.h"
+# include "../MLX42/include/MLX42/MLX42.h"
+// # define map_row 14
+// # define map_cols 33
 
 //check map
-// #define WIDTH 3000
-// #define HEIGHT 2000
+// # define WIDTH 3000
+// # define HEIGHT 2000
 
 //check 3D
-#define WIDTH 2000
-#define HEIGHT 1000
+# define WIDTH 2000
+# define HEIGHT 1000
 
+# define tile_size 64
 
-#define FOV_ANGLE M_PI / 3
-#define WALL_STRIP_WIDTH 1
-#define NUM_RAYS WIDTH
-#define line_leng 200
+# define FOV_ANGLE M_PI / 3
+# define WALL_STRIP_WIDTH 1
+# define NUM_RAYS WIDTH
+# define line_leng 200
 
-#define MAP_SCALE 1
+# define MAP_SCALE 1
 
+# define MINIMAP_SIZE 200
+# define MINIMAP_X_OFFSET 20
+# define MINIMAP_Y_OFFSET 20
 
-#define MINIMAP_SIZE 200
-#define MINIMAP_X_OFFSET 20
-#define MINIMAP_Y_OFFSET 20
-
-#define BLACK       0x000000
-#define WHITE       0xFFFFFF
-#define RED         0xFF0000FF
-#define GREEN       0x98FB98
-#define BLUE        0x0000FF
-#define GRAY        0xC0C0C0
-#define R_CLICK 76
-#define Y_CLICK 105
+# define BLACK       0x000000
+# define WHITE       0xFFFFFF
+# define RED         0xFF0000FF
+# define GREEN       0x98FB98
+# define BLUE        0x0000FF
+# define GRAY        0xC0C0C0
+# define R_CLICK 76
+# define Y_CLICK 105
 
 typedef struct s_doors
 {
-	int x;
-	int y;
-	int distance;
-	int state;
-	struct s_doors *next; 
+	int				x;
+	int				y;
+	int				distance;
+	int				state;
+	struct s_doors	*next;
 }	t_doors;
-
-
 
 typedef struct s_img
 {
@@ -64,204 +61,188 @@ typedef struct s_img
 	int		*arr;
 	int		width;
 	int		height;
-} t_img;
+}		t_img;
 
 typedef struct s_pos
 {
-    int x;
-    int y;
-}   t_pos;
+	int	x;
+	int	y;
+}		t_pos;
 
 typedef struct s_player
 {
 	double	player_x;
 	double	player_y;
 	double	player_z;
-	int	radius;
-	int turn_direction;
-	int strafe_direction;
-	int walk_direction;
-	int start;
-	double rotat_angle;
-	double rotation_speed;
-	double move_speed;
-	double is_moving_up;
-	double jump;
-	double jump_var;
-	int tab;
-	int middle;
-	int mouse;
-	int stop_mouse;
-	int right_left;
-	int shift;
-	int open;
+	int		radius;
+	int		turn_direction;
+	int		strafe_direction;
+	int		walk_direction;
+	int		start;
+	double	rotat_angle;
+	double	rotation_speed;
+	double	move_speed;
+	double	is_moving_up;
+	double	jump;
+	double	jump_var;
+	int		tab;
+	int		middle;
+	int		mouse;
+	int		stop_mouse;
+	int		right_left;
+	int		shift;
+	int		open;
+	int		minimap;
+
+
+	int	minimap_size;
+	int	minimap_start_x;
+	int	minimap_start_y;
+	int	minimap_player_x;
+	int	minimap_player_y;
 }	t_player;
 
-typedef struct s_texture {
-    void *img_ptr;
-    char *data;
-    int width;
-    int height;
-    int bpp;
-    int line_len;
-    int endian;
-} t_texture;
+typedef struct s_texture
+{
+	void	*img_ptr;
+	char	*data;
+	int		width;
+	int		height;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}		t_texture;
 
 typedef struct s_vars
 {
-	int isRayFacingDown;
-	int isRayFacingUp;
-	int isRayFacingRight;
-	int isRayFacingLeft;
-	int foundHorzWallHit;
-	int foundVertWallHit;
+	int		isRayFacingDown;
+	int		isRayFacingUp;
+	int		isRayFacingRight;
+	int		isRayFacingLeft;
+	int		foundHorzWallHit;
+	int		foundVertWallHit;
 
+	double	y_intercept;
+	double	x_intercept;
 
-	double y_intercept;
-	double x_intercept;
+	double	ystep;
+	double	xstep;
 
-	double ystep;
-	double xstep;
+	double	nextHorzTouchX;
+	double	nextHorzTouchY;
 
-	double nextHorzTouchX;
-	double nextHorzTouchY;
+	double	horzWallHitX;
+	double	horzWallHitY;
 
-	double horzWallHitX;
-	double horzWallHitY;
-	
+	double	vertWallHitX;
+	double	vertWallHitY;
 
-	double vertWallHitX;
-	double vertWallHitY;
+	double	nextVertTouchX;
+	double	nextVertTouchY;
 
-	double nextVertTouchX;
-	double nextVertTouchY;
+	double	horzHitDistance;
+	double	vertHitDistance;
 
-	double horzHitDistance;
-	double vertHitDistance;
+	double	wallHitX;
+	double	wallHitY;
 
-	double wallHitX;
-	double wallHitY;
-
-	double distance;
+	double	distance;
 	int		wasHitVert;
 
-	//door
-	int foundHorzDoorHit;
-	int foundVertDoorHit;
+	double	angle;
 
-	double horzDoorHitX;
-	double horzDoorHitY;
-	
-
-	double vertDoorHitX;
-	double vertDoorHitY;
-
-	double nextVertTouchX_Door;
-	double nextVertTouchY_Door;
-
-	double horzHitDistance_Door;
-	double vertHitDistance_Door;
-
-	double DoorHitX;
-	double DoorHitY;
-
-	double distance_Door;
-	int		wasHitVert_Door;
-
-
-	double angle;
-
-	int	textureNum;
-	double textureStep;
-	double textureOffsetY;
-	double wallBottomPixel;
-	double wallTopPixel;
-	int door;
+	int		textureNum;
+	double	textureStep;
+	double	textureOffsetY;
+	double	wallBottomPixel;
+	double	wallTopPixel;
+	int		door;
 }	t_vars;
 
 typedef struct s_minimap_vars
 {
-	double world_x;
-	double world_y;
-	int	map_x;
-	int	map_y;
-	int	color;
-	int	screen_x;
-	int	screen_y;
-	int	dx;
-	int	dy;
+	double	world_x;
+	double	world_y;
+	int		map_x;
+	int		map_y;
+	int		color;
+	int		screen_x;
+	int		screen_y;
+	int		dx;
+	int		dy;
 }	t_minimap_vars;
 
-typedef struct s_sprite {
-    double x;
-    double y;
-    mlx_image_t **images;
-    int num_images;
-    int current_image;
-    double animation_time;
-} t_sprite;
+typedef struct s_sprite
+{
+	double		x;
+	double		y;
+	mlx_image_t	**images;
+	int			num_images;
+	int			current_image;
+	double		animation_time;
+}		t_sprite;
 
 typedef struct s_threads
 {
-    int id;
-    pthread_t thread;
-}   t_threads;
+	int			id;
+	pthread_t	thread;
+}		t_threads;
 
 typedef struct s_cub
 {
-	char **map;
-	t_doors *doors_locations;
-	mlx_image_t* image;
-	mlx_t* mlx;
-	t_player *player;
-	mlx_texture_t* texture[4];
-	mlx_image_t* img[4];
-	mlx_texture_t* gun[Y_CLICK];
-	mlx_image_t* gun_img[Y_CLICK];
-	mlx_texture_t* gun_r[R_CLICK];
-	mlx_image_t* gun_r_img[R_CLICK];
-	mlx_texture_t* doors[1];
-	mlx_image_t* door_img[1];
-	char	**colors;
-	double		is;
-	int		y_press;
-	int		t_press;
-	int		right_press;
-	int current_gun_index;
-	int cur_g_right_clikc;
-	int	doortype;
-	t_minimap_vars map_data;
-	t_sprite *sprite;
-    double angle_0;
-    double angle_1;
-    pthread_mutex_t lock;
-	t_threads threads[4];
-	t_data	*data;
-} t_cub;
+	char			**map;
+	t_doors			*doors_locations;
+	mlx_image_t		*image;
+	mlx_t			*mlx;
+	t_player		*player;
+	mlx_texture_t	*texture[4];
+	mlx_image_t		*img[4];
+	mlx_texture_t	*gun[Y_CLICK];
+	mlx_image_t		*gun_img[Y_CLICK];
+	mlx_texture_t	*gun_r[R_CLICK];
+	mlx_image_t		*gun_r_img[R_CLICK];
+	mlx_texture_t	*doors[1];
+	mlx_image_t		*door_img[1];
+	char			**colors;
+	double			is;
+	int				y_press;
+	int				t_press;
+	int				right_press;
+	int				current_gun_index;
+	int				cur_g_right_clikc;
+	int				doortype;
+	t_minimap_vars	map_data;
+	t_sprite		*sprite;
+	double			angle_0;
+	double			angle_1;
+	pthread_mutex_t	lock;
+	t_threads		threads[4];
+	t_data			*data;
+}		t_cub;
 
-void    ft_fractol_init(t_cub *cube);
+void	ft_fractol_init(t_cub *cube);
 
 // hooks
-void loop_fun(void* param);
-void my_keyhook(mlx_key_data_t keydata, void* param);
+void	loop_fun(void *param);
+void	my_keyhook(mlx_key_data_t keydata, void *param);
 // end hooks
 //minimap
 void	draw_per(t_cub *cube);
-int create_rgba(int r, int g, int b, int a);
+int		create_rgba(int r, int g, int b, int a);
 
 //raycasting
-void DDA(t_cub *cube, double X0, double Y0, double X1, double Y1);
-int has_wall(t_cub *cube, double x1, double y1, int is);
-t_vars draw_line(t_cub *cube, double angle, int is);
-void draw_lines(t_cub *cube, int is);
-double normalizeAngle(double angle);
+void	DDA(t_cub *cube, double X0, double Y0, double X1, double Y1);
+int		has_wall(t_cub *cube, double x1, double y1, int is);
+t_vars	draw_line(t_cub *cube, double angle, int is);
+void	draw_lines(t_cub *cube, int is);
+double	normalizeAngle(double angle);
 
-void    ft_draw_player(t_cub *cube, int minimap_player_x, int minimap_player_y);
-void my_mousehook(mouse_key_t button, action_t action, modifier_key_t mods, void *param);
+void	ft_draw_player(t_cub *cube);
+void	my_mousehook(mouse_key_t button, action_t action, modifier_key_t mods, void *param);
 ///
 t_doors	*ft_lstnew_doors(int x, int y);
 void	ft_lstaddback_doors(t_doors **head, t_doors *node);
-int	ft_doors_size(t_doors *head);
+int		ft_doors_size(t_doors *head);
 void	ft_lstdel_doors(t_doors *lst);
 void	ft_lstclear_doors(t_doors **lst);
 #endif
