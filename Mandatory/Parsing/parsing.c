83f4989cb11ec52fad643ed11f18b17971e9a747
Mohamed Sarda <cbamiixsimo@gmail.com>
2024-08-27 20:11:10 +0100

    norm

--- Mandatory/Parsing/parsing.c
+++ Mandatory/Parsing/parsing.c
@@ -1,41 +1,21 @@
 #include "../../parsing.h"
 
-int	ft_count_cammas(char *ptr)
-{
-	int	i;
-	int	comma;
-
-	i = 0;
-	comma = 0;
-	if (!ptr)
-		return (-1);
-	while (ptr[i])
-	{
-		if (ptr[i] == ',' && (ptr[i + 1]
-				&& ptr[i + 1] >= '0' && ptr[i + 1] <= '9'))
-			comma++;
-		i++;
-	}
-	return (comma);
-}
-
 int	ft_get_color_data(t_init *init, t_data *data, int type)
 {
 	char	**colors;
-	char	**rgb;
 	int		i;
 	int		j;
 	t_color	*tmp;
 
 
 	colors = ft_split(init->colors[type], ' ', '\t');
+	if (!colors)
+		return (-1);
 	i = 0;
 	while (colors[i])
 		i++;
 	if (i != 2)
 		return (free_double_arr(colors), -1);
-	if (!colors)
-		return (-1);
 	if (ft_strlen(colors[0]) != 1)
 		return (free_double_arr(colors), ft_check_color_print(data), -1);
 	tmp = &data->floor;
@@ -44,54 +24,16 @@ int	ft_get_color_data(t_init *init, t_data *data, int type)
 	if (ft_count_cammas(colors[1]) != 2
 		|| colors[1][ft_strlen(colors[1]) - 1] == ',')
 		return (free_double_arr(colors), ft_check_color_print(data), -1);
-	j = 0;
-	i = 0;
-	while (colors[1][j])
-	{
-		if ((colors[1][j] >= '0' && colors[1][j] <= '9') || colors[1][j] == ',')
-			i++;
-		j++;
-	}
+	ft_count_num(&i, &j, colors);
 	if (i == j)
 	{
-		rgb = ft_split(colors[1], ',', 0);
-		if (!rgb)
-			return (-1);
-		tmp->r = ft_atoi(rgb[0]);
-		tmp->g = ft_atoi(rgb[1]);
-		tmp->b = ft_atoi(rgb[2]);
-		free_double_arr(rgb);
+		if (ft_fromdst_to_tmp(colors, tmp) == -1)
+			return (free_double_arr(colors), -1);
 	}
 	else
 		return (free_double_arr(colors), ft_check_color_print(data), -1);
 	free_double_arr(colors);
 	return (1);
-}
-
-void	ft_check_color(t_data *data)
-{
-	if ((data->floor.r < 0 || data->floor.r > 255)
-		|| (data->floor.g < 0 || data->floor.g > 255)
-		|| (data->floor.b < 0 || data->floor.b > 255)
-		|| (data->sky.r < 0 || data->sky.r > 255)
-		|| (data->sky.g < 0 || data->sky.g > 255)
-		|| (data->sky.b < 0 || data->sky.b > 255)
-	)
-		ft_check_color_print(data);
-}
-
-int	ft_check_char(char c, int is)
-{
-	if (is == 1)
-		return (c == '1' || c == '0' || c == 'N' || c == 'E'
-			|| c == 'W' || c == 'S' || c == 'D' || c == '$' || c == '\t');
-	if (is == 2)
-		return (c == 'N' || c == 'E' || c == 'W' || c == 'S');
-	if (is == 3)
-		return (c == '1' || c == '0' || c == 'N'
-			|| c == 'E' || c == 'W' || c == 'S' || c == 'D');
-	return (c == '0' || c == 'N' || c == 'E'
-		|| c == 'W' || c == 'S' || c == 'D');
 }
 
 int	ft_get_player(t_data *data)
@@ -136,136 +78,7 @@ int	ft_count_map_words(char **map)
 	return (counter);
 }
 
-int    ft_map_check_words(char **map)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (map[i])
-	{
-		j = 0;
-		while (map[i][j])
-		{
-			if (!ft_check_char(map[i][j], 1))
-				return (-1);
-			j++;
-		}
-		i++;
-	}
-	if (ft_count_map_words(map) != 1)
-		return (-1);
-	return (1);
-}
-
-int	check_only_space(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] && (str[i] == '\t' || str[i] == '$'))
-		i++;
-	if (str[i] != '\0')
-		return (0);
-	return (1);
-}
-
-int	ft_check_map(t_data *data)
-{
-	int	i;
-	int	j;
-	int	lines;
-
-	lines = 0;
-	while (data->map[lines])
-		lines++;
-	data->map_row = lines;
-	i = 0;
-	if (ft_map_check_words(data->map) == -1)
-		return (-1);
-	while (data->map[i])
-	{
-		j = 0;
-		while (data->map[i][j])
-		{
-			if (data->map[i][j] == '\t')
-				return (-1);
-			if (ft_check_char(data->map[i][j], 0)
-						&& (data->map[i][j + 1] == '$'
-					|| (j != 0 && data->map[i][j - 1] == '$')
-					|| j == 0 || j == (ft_strlen(data->map[i]) - 1)))
-				return (-1);
-			if (ft_check_char(data->map[i][j], 0) && ((i == (lines -1))
-				|| data->map[i + 1][j] == '$'
-				|| (i != 0 && data->map[i - 1][j] == '$') || i == 0))
-				return (-1);
-			j++;
-		}
-		i++;
-	}
-	return (1);
-}
-
-void	ft_change_map(t_init *init, t_data *data)
-{
-	int	i;
-	int	j;
-	int	len;
-	int	tmp_len;
-
-	tmp_len = 0;
-	len = 0;
-	i = 0;
-	j = 0;
-	data->map = (char **)ft_calloc((init->file_lines + 1) * sizeof(char *));
-	if (!data->map)
-		return ;
-	while (init->map[i])
-	{
-		tmp_len = ft_strlen(init->map[i]);
-		if (len < tmp_len)
-			len = tmp_len;
-		i++;
-	}
-	data->map_cols = len;
-	i = 0;
-	while (init->map[i])
-	{
-		j = 0;
-		data->map[i] = ft_calloc(len + 1);
-		if (!data->map[i])
-			return ;
-		while (init->map[i][j])
-		{
-			if (init->map[i][j] == ' ')
-				data->map[i][j] = '$';
-			else
-				data->map[i][j] = init->map[i][j];
-			j++;
-		}
-		while (j < len)
-			data->map[i][j++] = '$';
-		data->map[i][j] = 0;
-		i++;
-	}
-	data->map[i] = NULL;
-	return ;
-}
-
-void ft_open_files(t_data *data)
-{
-	if (open(data->ea, O_RDONLY) < 0
-		||open(data->so, O_RDONLY) < 0
-		|| open(data->we, O_RDONLY) < 0
-		|| open(data->no, O_RDONLY) < 0 )
-	{
-		ft_putstr_fd("Error: Please Check ", 2);
-		ft_putstr_fd("The Paths Provided For The Textures\n", 2);
-		exit(1);
-	}
-}
-
-int ft_get_data(t_init *init, t_data *data)
+int	ft_get_data(t_init *init, t_data *data)
 {
 	ft_open_files(data);
 	if (ft_get_color_data(init, data, 0) == -1
--- parsing.h
+++ parsing.h
@@ -43,10 +43,11 @@ typedef struct  s_data
 	int				width;
 	t_color			floor;
 	t_color			sky;
-}   t_data;
+}	t_data;
 
 
-int    ft_check_char(char c, int is);
+// parsing
+int		ft_count_map_words(char **map);
 
 //get next line
 int		ft_strchr(char *s, int c);
@@ -61,29 +62,44 @@ char	**ft_split(char *s, char c, char p);
 int		ft_words_counter(char *str, char c, char p);
 
 //file 1
-int	ft_atoi(char *str);
-int	ft_strncmp(char *s1, char *s2, size_t n);
-void    ft_putstr_fd(char *str, int fd);
+int		ft_atoi(char *str);
+int		ft_strncmp(char *s1, char *s2, size_t n);
+void	ft_putstr_fd(char *str, int fd);
 void	free_double_arr(char **tmp);
 
 //ft get init
-int ft_count_arrgs_in_file(t_init *init, char *key);
-void    ft_check_file(t_init *init);
-int	ft_check_2_cords_init(char **cords, t_data *data);
+int		ft_count_arrgs_in_file(t_init *init, char *key);
+void	ft_check_file(t_init *init);
+int		ft_check_2_cords_init(char **cords, t_data *data);
 
 //ft reading init
 
-int ft_get_data_init(t_init *init, t_data *data);
-int ft_get_coordinats_init(t_init *, t_data *datainit);
-int ft_get_colors_init(t_init *init);
-int ft_get_data(t_init *init, t_data *data);
+int		ft_get_data_init(t_init *init, t_data *data);
+int		ft_get_coordinats_init(t_init *init, t_data *datainit);
+int		ft_get_colors_init(t_init *init);
+int		ft_get_data(t_init *init, t_data *data);
 
 //error 1
 
 void	ft_check_color_print(t_data *data);
 void	ft_print_cords_err(t_init *init);
-void    ft_check_file(t_init *init);
+void	ft_check_file(t_init *init);
 void	ft_check_map_print(t_data *data);
 
+// ft_colors
+
+int		ft_count_cammas(char *ptr);
+int		ft_fromdst_to_tmp(char **colors, t_color *tmp);
+void	ft_count_num(int *i, int *j, char **colors);
+int		ft_check_char(char c, int is);
+void	ft_check_color(t_data *data);
+
+// ft_map
+
+int		ft_check_map(t_data *data);
+void	ft_change_map(t_init *init, t_data *data);
+void	ft_open_files(t_data *data);
+int		ft_map_check_words(char **map);
+int		check_only_space(char *str);
 
 #endif
